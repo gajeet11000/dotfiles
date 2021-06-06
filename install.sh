b116544cfa7fac400978c2e48712c9da3836fbd1
G Ajeet <gajeet11000@gmail.com>
2021-06-06 22:54:21 +0530

    update config files for keyboard shortcuts

--- install.sh
+++ install.sh
@@ -4,11 +4,12 @@ sudo pacman -S yay
 
 yay -S safeeyes teamviewer fisher
 
-sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv pamac-flatpak-plugin pamac-snap-plugin xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium redshift jre-openjdk jre-openjdk-headless fish nomacs xournalpp bitwarden pinta
+sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv pamac-flatpak-plugin pamac-snap-plugin xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium jre-openjdk jre-openjdk-headless fish nomacs xournalpp bitwarden pinta
 
-fisher install IlanCosman/tide
 
 cp -r .config/ .fonts/ .icons/ .moc/ .themes/ .vim/ .xdman .gvimrc .vimrc .gitconfig .bashrc ~/
 
 sudo chsh -s /usr/bin/fish
 chsh -s /usr/bin/fish
+
+fisher install IlanCosman/tide
