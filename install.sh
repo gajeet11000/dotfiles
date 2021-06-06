b03fa97b76d0cb484a8a0fcdb64a2f6775cbcd24
G Ajeet <gajeet11000@gmail.com>
2021-06-06 14:09:35 +0530

    update install.sh with fisher

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@ sudo pacman -R --noconfirm blueman midori parole mousepad
 
 sudo pacman -S yay
 
-yay -S safeeyes teamviewer
+yay -S safeeyes teamviewer fisher
 
-sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv pamac-flatpak-plugin pamac-snap-plugin xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium redshift jre-openjdk jre-openjdk-headless fish nomacs xournalpp bitwarden pinta 
+sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv pamac-flatpak-plugin pamac-snap-plugin xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium redshift jre-openjdk jre-openjdk-headless fish nomacs xournalpp bitwarden pinta
+
+fisher install IlanCosman/tide
 
 cp -r .config/ .fonts/ .icons/ .moc/ .themes/ .vim/ .xdman .gvimrc .vimrc .gitconfig .bashrc ~/
 
