799cca3e2fcc1ee19376aa0b81096ac1ffd5ceed
G Ajeet <gajeet11000@gmail.com>
2021-06-07 11:45:12 +0530

    update config fish terminal

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ alias c="clear"
 alias aq="asciiquarium"
 alias cm="cmatrix"
 alias cs="fortune | cowsay | lolcat"
+alias songs="mocp"
+alias ls="lsd -a"
 
 ### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
 set fish_color_normal brcyan
