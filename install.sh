4447375142ca4fa79e1fd986887a12928ece68de
G Ajeet <gajeet11000@gmail.com>
2021-06-08 09:18:21 +0530

    Latest update

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
+sudo pacman -Syyy
+
+sudo pacman -Syu
+
+sudo pacman -S --noconfirm base-devel
+
+pamac build safeeyes
+pamac build teamviewer
+pamac build fisher 
+pamac build gammy
+
+sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv pamac-flatpak-plugin pamac-snap-plugin xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium jre-openjdk jre-openjdk-headless fish nomacs xournalpp bitwarden pinta lsd
+
+cp -r .config/ .fonts/ .icons/ .moc/ .themes/ .vim/ .xdman .gvimrc .vimrc .bashrc ~/
+
 sudo pacman -R --noconfirm blueman midori parole mousepad
-
-sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv pamac-flatpak-plugin pamac-snap-plugin xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium jre-openjdk jre-openjdk-headless fish nomacs xournalpp bitwarden pinta base-devel yay lsd
-
-#sudo pamcan -S --noconfirm code
-
-yay -S safeeyes teamviewer fisher gammy
-
-cp -r .config/ .fonts/ .icons/ .moc/ .themes/ .vim/ .xdman .gvimrc .vimrc .gitconfig .bashrc ~/
 
 sudo chsh -s /usr/bin/fish
 chsh -s /usr/bin/fish
@@ -14,4 +21,5 @@ chsh -s /usr/bin/fish
 fisher install IlanCosman/tide
 
 cd /home/gajeet/Documents/
+
 git clone https://github.com/gajeet11000/Apna-College-DSA-Practice-Questions.git
