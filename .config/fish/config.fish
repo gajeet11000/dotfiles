9e6fc90fded437bb4668e63fa8d12d9ca8d82f0a
G Ajeet <gajeet11000@gmail.com>
2021-07-22 12:13:19 +0530

    mpv config files updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,9 +7,9 @@ alias songs="mocp"
 alias ls="lsd -a"
 
 ### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
-set fish_color_normal brcyan
-set fish_color_autosuggestion '#7d7d7d'
-set fish_color_command brcyan
-set fish_color_error '#ff6c6b'
-set fish_color_param brcyan
+#set fish_color_normal brcyan
+#set fish_color_autosuggestion '#7d7d7d'
+#set fish_color_command brcyan
+#set fish_color_error '#ff6c6b'
+#set fish_color_param brcyan
 
