930e39ee6ab59213c1f4644b8f1f129578cacd74
G Ajeet <gajeet11000@gmail.com>
2021-10-17 20:55:35 +0530

    add nemo file manager in install list

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ sudo pacman -S --noconfirm base-devel
 
 pamac build safeeyes
 pamac build teamviewer
-pamac build fisher 
-pamac build gammy postman-bin heroku-cli
+pamac build gammy
 
-sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv npm xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium fish nomacs xournalpp bitwarden pinta lsd skippy-xd
+sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium fish nomacs xournalpp bitwarden pinta lsd skippy-xd nemo nemo-engrampa fisher yay
 
 cp -r .config/ .fonts/ .icons/ .moc/ .themes/ .vim/ .local/ .xdman/ .gvimrc .vimrc .bashrc ~/
 
@@ -19,11 +18,3 @@ sudo chsh -s /usr/bin/fish
 chsh -s /usr/bin/fish
 
 fisher install IlanCosman/tide
-
-cd /home/gajeet/Documents/
-
-git clone https://github.com/gajeet11000/Apna-College-DSA-Practice-Questions.git
-
-sudo mount /dev/sda4 /mnt/Ajeet/
-
-sudo sh /mnt/Ajeet/SOFTWARE/Linux\ Softwares/xdm-setup-7.2.11/install.sh
