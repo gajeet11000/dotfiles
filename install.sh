d653d274065e57b686024982f69ac3f18aa561f2
G Ajeet <gajeet11000@gmail.com>
2021-10-26 23:41:12 +0530

    add flameshot package to install

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ pamac build safeeyes
 pamac build teamviewer
 pamac build gammy
 
-sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium fish nomacs xournalpp bitwarden pinta lsd skippy-xd nemo nemo-engrampa fisher yay dconf-editor
+sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium fish nomacs xournalpp bitwarden pinta lsd skippy-xd nemo nemo-engrampa fisher yay dconf-editor flameshot
 
 cp -r .config/ .fonts/ .icons/ .moc/ .themes/ .vim/ .local/ .xdman/ .gvimrc .vimrc .bashrc ~/
 
