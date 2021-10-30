fbcf44ce5d3e7b7ee2efca2606073cf763bc7960
G Ajeet <gajeet11000@gmail.com>
2021-10-30 13:36:29 +0530

    modified install bash script

--- install.sh
+++ install.sh
@@ -4,17 +4,15 @@ sudo pacman -Syu
 
 sudo pacman -S --noconfirm base-devel
 
-pamac build safeeyes
-pamac build teamviewer
-pamac build gammy
-
 sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium fish nomacs xournalpp bitwarden pinta lsd skippy-xd nemo nemo-engrampa fisher yay dconf-editor flameshot
+
+yay -S --noconfirm safeeyes teamviewer gammy
 
 cp -r .config/ .fonts/ .icons/ .moc/ .themes/ .vim/ .local/ .xdman/ .gvimrc .vimrc .bashrc ~/
 
-sudo pacman -R --noconfirm blueman midori parole mousepad
+sudo pacman -R --noconfirm midori parole mousepad
+
+fisher install IlanCosman/tide
 
 sudo chsh -s /usr/bin/fish
 chsh -s /usr/bin/fish
-
-fisher install IlanCosman/tide
