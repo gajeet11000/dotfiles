3eb847c9d62702212505b3801f08a0b73a7a3132
G Ajeet <gajeet11000@gmail.com>
2021-10-30 14:35:22 +0530

    add code and sublime-text-4

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ sudo pacman -Syu
 
 sudo pacman -S --noconfirm base-devel
 
-sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium fish nomacs xournalpp bitwarden pinta lsd skippy-xd nemo nemo-engrampa fisher yay dconf-editor flameshot
+sudo pacman -S --noconfirm firefox gimp telegram-desktop mpv xfce4-netload-plugin rofi gvim gcc gnome-disk-utility mintstick moc cowsay fortune-mod cmatrix lolcat otf-cascadia-code deluge-gtk xorg-xbacklight gnome-calculator asciiquarium fish nomacs xournalpp bitwarden pinta lsd skippy-xd nemo nemo-engrampa fisher yay dconf-editor flameshot code
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-yay -S --noconfirm safeeyes teamviewer gammy
+yay -S --noconfirm safeeyes teamviewer gammy sublime-text-4
 
 cp -r .config/ .fonts/ .icons/ .moc/ .themes/ .vim/ .local/ .xdman/ .gvimrc .vimrc .bashrc ~/
 
