72a3925627ab158b3f437de5e4366a99f307c7f3
G Ajeet <gajeet11000@gmail.com>
2023-10-13 21:46:25 +0530

    add vim keybindings in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,7 @@ alias c="clear"
 alias aq="asciiquarium"
 alias cm="cmatrix"
 alias cs="fortune | cowsay | lolcat"
-alias songs="mocp"
 alias ls="lsd -a"
+
+fish_vi_key_bindings
+bind -M insert -m default ii backward-char force-repaint
