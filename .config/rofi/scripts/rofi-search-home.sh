7432fb8b2250722409c83a1bc190e2d2d95b5f79
G Ajeet <gajeet11000@gmail.com>
2023-10-16 15:46:35 +0530

    git modify escape sequence for & and () symbols

--- .config/rofi/scripts/rofi-search-home.sh
+++ .config/rofi/scripts/rofi-search-home.sh
@@ -3,7 +3,6 @@ opener="xdg-open"
 selection=$(fd . --hidden $HOME 2>/dev/null | \
     sed 's;$HOME;~;' | \
     rofi -sort -sorting-method fzf -dmenu -i -theme ./themes/search -no-custom -p "Search:" | \
-    sed 's;\~;$HOME; ;s/ /\\ /g'
+    sed 's;\~;$HOME; ;s/ /\\ /g; s/\&/\\&/g; s/(/\\(/g; s/)/\\)/g'
 )
-
 eval $opener "$selection" 
--- .config/rofi/scripts/rofi-search-storage.sh
+++ .config/rofi/scripts/rofi-search-storage.sh
@@ -2,7 +2,7 @@ opener="xdg-open"
 
 selection=$(fd . --hidden /mnt/Ajeet 2>/dev/null | \
     rofi -sort -sorting-method fzf -dmenu -i -theme ./themes/search -no-custom -p "Search:" | \
-    sed 's/ /\\ /g'
+    sed 's/ /\\ /g; s/\&/\\&/g; s/(/\\(/g; s/)/\\)/g'
 )
 
 eval $opener "$selection"
