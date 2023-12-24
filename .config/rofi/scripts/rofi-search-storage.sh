d32d6229388ca19039c4b363b6f98e77dfad2035
G Ajeet <gajeet11000@gmail.com>
2023-12-25 00:13:51 +0530

    added rofi new searching bash scripts

--- .config/rofi/scripts/rofi-search-storage.sh
+++
@@ -1,8 +0,0 @@
-opener="xdg-open"
-
-selection=$(fd . --hidden /mnt/Ajeet 2>/dev/null | \
-    rofi -sort -sorting-method fzf -dmenu -i -theme ./themes/search -no-custom -p "Search:" | \
-    sed 's/ /\\ /g; s/\&/\\&/g; s/(/\\(/g; s/)/\\)/g'
-)
-
-eval $opener "$selection"
