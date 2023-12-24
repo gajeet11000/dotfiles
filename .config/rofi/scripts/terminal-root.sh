e35819111d20e85c0afdb2a053a58964d9abf5eb
G Ajeet <gajeet11000@gmail.com>
2023-12-25 01:21:04 +0530

    added only directories flag for open in terminal bash scripts

--- .config/rofi/scripts/terminal-root.sh
+++ .config/rofi/scripts/terminal-root.sh
@@ -3,7 +3,7 @@
 opener="kitty"
 
 if [[ -z "$1" ]]; then
-    fd . --hidden / --exclude /home --exclude /mnt --exclude /lost+found --exclude /root 2>/dev/null"
+    fd . --hidden --type d / --exclude /home --exclude /mnt --exclude /lost+found --exclude /root 2>/dev/null
 else
     selection=$(echo "$1" | sed 's/ /\\ /g; s/\&/\\&/g; s/(/\\(/g; s/)/\\)/g')
     eval $opener "$selection" > /dev/null 2>&1 &
--- .config/rofi/scripts/terminal-storage.sh
+++ .config/rofi/scripts/terminal-storage.sh
@@ -3,7 +3,7 @@
 opener="kitty"
 
 if [[ -z "$1" ]]; then
-    fd . --hidden /mnt/Storage 2>/dev/null
+    fd . --hidden --type d /mnt/Storage 2>/dev/null
 else
     selection=$(echo "$1" | sed 's/ /\\ /g; s/\&/\\&/g; s/(/\\(/g; s/)/\\)/g')
     eval $opener "$selection" > /dev/null 2>&1 &
