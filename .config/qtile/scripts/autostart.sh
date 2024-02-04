a556c773d70788d1e7cf377da785336edf5a56f7
G Ajeet <gajeet11000@gmail.com>
2024-02-05 00:28:09 +0530

    Update autostart.sh
    removed unnecessary codes

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -7,9 +7,6 @@ function run {
   fi
 }
 
-#Set your native resolution IF it does not exist in xrandr
-#More info in the script
-#run $HOME/.config/qtile/scripts/set-screen-resolution-in-virtualbox.sh
 
 #Find out your monitor name with xrandr or arandr (save and you get this line)
 #xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
@@ -18,57 +15,28 @@ function run {
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 #autorandr horizontal
 
-#change your keyboard if you need it
-#setxkbmap -layout be
-
-# keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
-
-# if [ $keybLayout = "be" ]; then
-#   cp $HOME/.config/qtile/config-azerty.py $HOME/.config/qtile/config.py
-# fi
-
-#autostart ArcoLinux Welcome App
-run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop &
-
 #Some ways to set your wallpaper besides variety or nitrogen
 # feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
 # feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 #wallpaper for other Arch based systems
 feh --bg-fill -z /mnt/Storage/PHOTOS/Wallpapers &
-#start the conky to learn the shortcuts
-#(conky -c $HOME/.config/qtile/scripts/system-overview) &
 
-#start sxhkd to replace Qtile native key-bindings
-#run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
-
-
-#starting utility applications at boot time
-# run variety &
 if [[ ! `pidof xfce-polkit` ]]; then
 	/usr/lib/xfce-polkit/xfce-polkit &
 fi
-run kdeconnect-indicator &
+
+exec dunst &
+exec thunar --daemon &
+xfsettingsd &
 run nm-applet &
-run pamac-tray &
 run xfce4-power-manager &
-numlockx on &
-blueberry-tray &
+run numlockx on &
+blueman-applet &
+
 picom --config $HOME/.config/picom/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
 
 #starting user applications at boot time
 run copyq &
 run volumeicon &
 run flameshot &
-#run discord &
-#nitrogen --restore &
-#run caffeine -a &
-#run vivaldi-stable &
-#run firefox &
-#run thunar &
-#run dropbox &
-#run insync start &
-#run spotify &
-#run atom &
-#run telegram-desktop &
+run kdeconnect-indicator &
